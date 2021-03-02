7f333d0450a523c5dd06a4f38a23b54398a7c315
Jon Oliver <jon@heysparkbox.com>
2021-03-02 00:33:41 -0500

    o/ functional bob

--- bob.js
+++ bob.js
@@ -1,8 +1,27 @@
-//
-// This is only a SKELETON file for the 'Bob' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const isQuiet = (message) => !message;
 
-export const hey = (message) => {
-  throw new Error('Remove this statement and implement this function');
-};
+const isQuestion = (message) => message.endsWith("?");
+
+const isUpperCase = (message) =>
+  message.match(/[A-Za-z]/) &&
+  message === message.toUpperCase();
+
+const isYelling = (message) =>
+  !isQuiet(message) && isUpperCase(message);
+
+const isYellingQuestion = (message) =>
+  isYelling(message) && isQuestion(message);
+
+const isDefault = () => true;
+
+const responses = [
+  [isQuiet, "Fine. Be that way!"],
+  [isYellingQuestion, "Calm down, I know what I'm doing!"],
+  [isQuestion, "Sure."],
+  [isYelling, "Whoa, chill out!"],
+  [isDefault, "Whatever."]
+];
+
+const selectResponse = (message) => responses.find(([condition]) => condition(message))[1];
+
+export const hey = (message) => selectResponse(message.trim());
