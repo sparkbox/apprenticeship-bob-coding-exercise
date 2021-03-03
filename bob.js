83bd5bd7b260c9a2a655d41be3bddb3d560ecaf5
Sheridan McKisick <sheridan@heysparkbox.com>
2021-03-03 14:42:54 -0500

    Added all possible responses as variables

--- bob.js
+++ bob.js
@@ -3,6 +3,12 @@
 // convenience to get you started writing code faster.
 //
 
+const normalResponse = 'Whatever.'
+const shoutResponse = 'Whoa, chill out!'
+const questionResponse = 'Sure.'
+const shoutQuestionResponse = 'Calm down, I know what I\'m doing!'
+const silentResponse = 'Fine. Be that way!'
+
 export const hey = (message) => {
   throw new Error('Remove this statement and implement this function');
 };
