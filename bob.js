bfff84d06b5602de2b839853b7b1bf68e3d900cb
Sheridan McKisick <sheridan@heysparkbox.com>
2021-03-04 10:57:28 -0500

    Added conditions for shouting something that is not a question

--- bob.js
+++ bob.js
@@ -11,8 +11,17 @@ const silentResponse = 'Fine. Be that way!'
 
 const includesCharacters = (message) => /\S/.test(message)
 
+const includesWords = (message) => /[A-z]/.test(message)
+
+const allOneCase = (message) => message.toUpperCase() === message
+
+const isStatement = (message) => !message.endsWith('?')
+
 export const hey = (message) => {
   if (!includesCharacters(message)) {
     return silentResponse
+  }
+  if (includesWords(message) && allOneCase(message) && isStatement(message)) {
+    return shoutResponse
   }
 };
