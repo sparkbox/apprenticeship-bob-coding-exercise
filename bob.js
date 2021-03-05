52198bac91ecf38ae7c0b72e03c8414cdc875488
Jon Oliver <jon@heysparkbox.com>
2021-03-05 14:37:30 -0500

    another OOP approach

--- bob.js
+++ bob.js
@@ -1,43 +1,42 @@
-const isQuiet = (message) => !message;
-
-const isQuestion = (message) => message.endsWith("?");
-
-const isUpperCase = (message) =>
-  message.match(/[A-Za-z]/) &&
-  message === message.toUpperCase();
-
-const isYelling = (message) =>
-  !isQuiet(message) && isUpperCase(message);
-
-const isYellingQuestion = (message) =>
-  isYelling(message) && isQuestion(message);
-
-const isDefault = () => true;
-
-function Response(condition, responseMessage) {
-  return {
-    isMatch(message) {
-      return condition(message);
-    },
-    responseMessage
+class MessageParser {
+  constructor(message){
+    this.message = message
   }
+
+  isQuiet = () => !this.message;
+
+  isQuestion = () => this.message.endsWith("?");
+
+  isUpperCase = () =>
+    this.message.match(/[A-Za-z]/) &&
+    this.message === this.message.toUpperCase();
+
+  isYelling = () =>
+    !this.isQuiet() && this.isUpperCase();
+
+  isYellingQuestion = () =>
+    this.isYelling(this.message) && this.isQuestion(this.message);
+
+  isDefault = () => true;
 }
 
 const responses = [
-  new Response(isQuiet, "Fine. Be that way!"),
-  new Response(isYellingQuestion, "Calm down, I know what I'm doing!"),
-  new Response(isQuestion, "Sure."),
-  new Response(isYelling, "Whoa, chill out!"),
-  new Response(isDefault, "Whatever.")
+  ['isQuiet', "Fine. Be that way!"],
+  ['isYellingQuestion', "Calm down, I know what I'm doing!"],
+  ['isQuestion', "Sure."],
+  ['isYelling', "Whoa, chill out!"],
+  ['isDefault', "Whatever."],
 ];
 
-function Person(responses){
+function Person(responses, parser){
   this.responses = responses;
+  this.parser = parser
   this.respond = function(message){
-    return responses.find(response => response.isMatch(message)).responseMessage;
+    const parsedMessage = new this.parser(message)
+    return responses.find(([ condition ]) => parsedMessage[condition]())[1];
   }
 }
 
-const bob = new Person(responses);
+const bob = new Person(responses, MessageParser);
 
 export const hey = (message) => bob.respond(message.trim());
